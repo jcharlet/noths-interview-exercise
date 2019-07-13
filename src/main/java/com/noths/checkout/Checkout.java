51d5b6075d0f6db69c42f4b7ad3d1e1cfa982b4a
Jeremie <charlet.jeremie@gmail.com>
2019-07-13 16:49:33 +0200

    upd: refactor, moved initialization of maps in constructor

--- src/main/java/com/noths/checkout/Checkout.java
+++ src/main/java/com/noths/checkout/Checkout.java
@@ -6,35 +6,19 @@ import com.noths.checkout.domain.PromotionalRule;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Checkout {
 
-    private final List<PromotionalRule> promotionalRules;
+    private Map<String, PromotionalRule> productPromotionalRules = new HashMap<>();
+    private List<PromotionalRule> totalPromotionalRules = new ArrayList<>();
+
     private Map<Product,Integer> scannedProductsMap = new HashMap<>();
 
     public Checkout(List<PromotionalRule> promotionalRules) {
-        this.promotionalRules=promotionalRules;
-    }
-
-    public void scan(Product product) {
-        Integer numberOfItems = scannedProductsMap.get(product);
-        if(numberOfItems==null){
-            numberOfItems=0;
-        }
-        scannedProductsMap.put(product,numberOfItems+1);
-    }
-
-    public Float total() {
-        BigDecimal total = new BigDecimal("0");
-        Map<String,PromotionalRule> productPromotionalRules = null;
-        List<PromotionalRule> totalPromotionalRules = null;
-        if(promotionalRules!=null){
+        if(promotionalRules!=null) {
             productPromotionalRules = promotionalRules.stream()
                     .filter(product -> product.getPromotionType()== PromotionType.PRODUCT)
                     .collect(Collectors.toMap(PromotionalRule::getProductCode,Function.identity()));
@@ -44,19 +28,31 @@ public class Checkout {
                     .collect(Collectors.toList());
             totalPromotionalRules.sort(Comparator.comparingDouble(PromotionalRule::getThreshold).reversed());
         }
+    }
+
+    public void scan(Product product) {
+        Integer nbItems = scannedProductsMap.get(product);
+        if(nbItems==null){
+            nbItems=0;
+        }
+        scannedProductsMap.put(product,nbItems+1);
+    }
+
+    public Float total() {
+        BigDecimal total = new BigDecimal("0");
 
         for(Product product: scannedProductsMap.keySet()){
-            Integer numberOfItems = scannedProductsMap.get(product);
+            Integer nbItems = scannedProductsMap.get(product);
             Float price = product.getPrice();
-            if(productPromotionalRules!=null){
+            if(!productPromotionalRules.isEmpty()){
                 PromotionalRule rule = productPromotionalRules.get(product.getProductCode());
-                if(rule!=null && numberOfItems>=rule.getThreshold()){
+                if(rule!=null && nbItems>=rule.getThreshold()){
                     price=rule.getDiscount();
                 }
             }
-            total= total.add(new BigDecimal(price.toString()).multiply(new BigDecimal(numberOfItems.toString())));
+            total= total.add(new BigDecimal(price.toString()).multiply(new BigDecimal(nbItems.toString())));
         }
-        if(totalPromotionalRules!=null) {
+        if(!totalPromotionalRules.isEmpty()) {
             for (PromotionalRule rule : totalPromotionalRules) {
                 if (total.compareTo(new BigDecimal(rule.getThreshold().toString()))>0) {
                     BigDecimal discountedPrice = total.multiply(new BigDecimal(Float.toString(1 - rule.getDiscount())));
